43e096f4034ff2d68cd65d932f8dcd9992adb3b2
Adrian Badicu <badicuady@gmail.com>
2024-02-15 17:00:29 +0200

    Update with docker compose file

--- apps/node/start.ps1
+++ apps/node/start.ps1
@@ -1,3 +1,3 @@
 docker remove -f node
 docker build --tag node:latest .
-docker run --name node --rm -d -p 3000:3000 --net net node:latest
+docker run --name node --rm -d -p 3100:3100 --net net node:latest
